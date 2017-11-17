4cb2103f72dbdbe9bdaba70b397e6774190fee7a
Barnabas_Chan <Barny@CC-2s-MacBook-Pro.local>
2017-11-17 13:41:18 +0800

    [first commit]

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack');
+var path = require('path');
+
+// Naming and path settings
+var appName = 'app';
+var entryPoint = './src/main.js';
+var exportPath = path.resolve(__dirname, './build');
+
+// Environment flag
+var plugins = [];
+var env = process.env.WEBPACK_ENV;
+
+// Differ settings based on production flag
+if (env === 'production') {
+    var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    plugins.push(new webpack.DefinePlugin({
+        'process.env' {
+            NODE_ENV: '"production"'
+        }
+    }));
+
+    appName = appName + '.min.js';
+} else {
+    appName = appName + '.js';
+}
+
+// Main settings config
+module.exports = {
+    entry: entryPoint,
+    output: {
+        path: exportPath,
+        filename: appName
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins
+};
